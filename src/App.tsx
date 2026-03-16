eaed336c52287046e420e9584ac1d0a9ed8ba43f
Sean Marcia <smarcia@gmail.com>
2026-03-15 23:43:21 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import PreviousOfficers from './pages/PreviousOfficers';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
